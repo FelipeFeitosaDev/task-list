4e54226c4a647a4dfec2b4bf46936a0e53b78ae4
Felipe Feitosa <pro.felipefeitosa@gmail.com>
2021-10-19 10:33:53 -0300

    functions has been created

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,5 +6,53 @@ const inputElement = document.querySelector('.input-task');
 const addBtn = document.querySelector('.add');
 const listElement = document.querySelector('.list-tasks');
 
+// 2. creating functions:  
+getStorage();
+
+function createElement(stringElement, className, txtContent = ''){
+    const element = document.createElement(stringElement);
+    element.innerText = txtContent;
+    element.classList.add(className);
+    return element;
+}
+
+function createTask(task){
+    const li = createElement('li', 'item');
+    const button = createElement('button','delete','x');
+
+    li.appendChild(button);
+    li.innerHTML += task;
+
+    listElement.appendChild(li);
+    saveStorage();
+}
+
+function clearInput(){
+    inputElement.value = '';
+    inputElement.focus();
+}
+
+function saveStorage(){
+    const itensElements = listElement.querySelectorAll('.item');
+    const taskList = [];
+
+    for(let task of itensElements){
+        let taskTxt = task.innerText.replace('x','');
+        taskList.push(taskTxt);
+    }
+
+    const JSONlist = JSON.stringify(taskList);
+    localStorage.setItem('Tasks:', JSONlist);
+}
+
+function getStorage(){
+    const JSONlist = localStorage.getItem('Tasks:');
+    const storageList = JSON.parse(JSONlist);
+
+    for(let task of storageList){
+        createTask(task);
+    }
+}
+
 }
 escope();
