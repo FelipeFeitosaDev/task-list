5c53f62c2b960b91424167415e2807bcc605d1ed
Felipe Feitosa <pro.felipefeitosa@gmail.com>
2021-10-19 10:33:02 -0300

    the tag-elements have been accessed

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+function escope(){
+
+// 1. acessing elements:
+
+const inputElement = document.querySelector('.input-task');
+const addBtn = document.querySelector('.add');
+const listElement = document.querySelector('.list-tasks');
+
+}
+escope();
